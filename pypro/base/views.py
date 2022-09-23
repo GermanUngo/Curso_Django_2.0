7cb7777968875996b5ffa63de972623676a98f29
GERMAN IGNACIO UNGO CORRALES <germanungo@gmail.com>
2022-09-23 11:49:03 -0300

    Criada infra de teste com pytest-django

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
 
 
 def home(request):
-    return HttpResponse('Olá Francesca')
+    return HttpResponse('Olá Django')
