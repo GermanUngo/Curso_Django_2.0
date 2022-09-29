2fa79677353aa5ca1db75ec9169148ec85ad1d34
GERMAN IGNACIO UNGO CORRALES <germanungo@gmail.com>
2022-09-29 16:17:03 -0300

    Foi criado um usuário na Amazon para poder fazer o upload de arquivos para o S3.

--- pypro/settings.py
+++ pypro/settings.py
@@ -18,6 +18,7 @@ from decouple import Csv, Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from decouple import config
+from django.conf.global_settings import STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -111,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -122,10 +123,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+#Configuração de ambiente de desenvolvimento
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
