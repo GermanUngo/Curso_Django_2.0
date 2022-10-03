5620de59441304a00c01178bf5e9df00a2e7e819
GERMAN IGNACIO UNGO CORRALES <germanungo@gmail.com>
2022-10-03 18:49:39 -0300

    Customizando a Classe UserManager

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -1,9 +1,40 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager)
+    use_in_migrations = True
+
+    def _create_user(self, email, pasword, **extra_fields):
+        """
+        creates and saves a User with the given email and pasword.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields):
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, pasword=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, pasword, **extra_fields)
+
+    def create_superuser(self, email, pasword, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superiser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+
+        return self._create_user(email, pasword, **extra_fields)
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """
