387da9fe14585b10cbb2c4e29c6bbd40f682f07f
GERMAN IGNACIO UNGO CORRALES <germanungo@gmail.com>
2022-10-11 14:34:48 -0300

    Configurando o Projeto no app SENTRY

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,6 +22,9 @@ from decouple import Csv, Config
 from decouple import config
 from django.conf.global_settings import STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -187,4 +190,21 @@ if AWS_ACCESS_KEY_ID:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+SENTRY_DSN=config('SENTRY_DSN', default=None)
 
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[
+            DjangoIntegration(),
+        ],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
