d61c4bbd3af04d993314e3f16c1f3ade9784a981
GERMAN IGNACIO UNGO CORRALES <germanungo@gmail.com>
2022-10-25 17:16:52 -0300

    Feita a criação da migração inicial relativa à app de aperitivos

--- pypro/aperitivos/models.py
+++ pypro/aperitivos/models.py
@@ -3,9 +3,10 @@ from django.urls import reverse
 
 
 class Video(models.Model):
-    slug = models.CharField(max_length=32)
-    titulo = models.CharField(max_length=32)
-    vimeo_id = models.CharField(max_length=32)
+    slug = models.CharField(max_length=50)
+    titulo = models.CharField(max_length=50)
+    vimeo_id = models.CharField(max_length=50)
+    creation = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from pypro.aperitivos.models import Video
 
-videos = [
-    Video(slug='belezadopython', titulo='Video Aperitivo: A Beleza de Python', vimeo_id='760852969'),
-    Video(slug='POO', titulo='Programação Orientada a Objetos', vimeo_id='760876240'),
-]
-
-videos_dct = {v.slug: v for v in videos}
-
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
