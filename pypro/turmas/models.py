c0c09f4ee2a2f7b6f9eb71318e091beedae29233
GERMAN IGNACIO UNGO CORRALES <germanungo@gmail.com>
2022-11-03 19:59:48 -0300

    Feita a configuração de relacionamento de muitas para muitos com o campo ManyToManyField.

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+
+
+class Turma(models.Model):
+    nome = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64)
+    inicio = models.DateField()
+    fim = models.DateField()
+    matriculas = models.ManyToManyField(get_user_model())
